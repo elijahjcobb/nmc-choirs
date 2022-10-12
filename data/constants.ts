06e1d97ff147f5c21c144fcf0f8cbf6c0588431c
Elijah Cobb <elijah.cobb@vercel.com>
2022-10-12 16:04:31 -0400

    fixed revalidate time

--- data/constants.ts
+++ data/constants.ts
@@ -1,2 +1,2 @@
-export const REVALIDATE_TIME = 60 * 3;
+export const REVALIDATE_TIME = 3;
 export const FALLBACK_MODE = "blocking";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { GetStaticProps } from "next";
 import { Dir, type DirProps } from "../components/dir";
+import { REVALIDATE_TIME } from "../data/constants";
 import { fetchFilesInDirectory } from "../data/drive";
 
 
@@ -12,6 +13,7 @@ export const getStaticProps: GetStaticProps<DirProps> = async () => {
 		props: {
 			directory: await fetchFilesInDirectory(),
 			name: "Home"
-		}
+		},
+		revalidate: REVALIDATE_TIME,
 	}
 }
